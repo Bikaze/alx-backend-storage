1c00273ebc5d4c0c4111a941ad0a9f88bd9bd938
Bikaze <clmntmugisha@gmail.com>
2024-07-13 19:46:00 +0200

    Create 11-need_meeting.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,9 +1,5 @@
 -- This script creates a function SafeDiv that divides (and returns)
 -- the first by the second number or returns 0 if the second number is equal to 0.
--- The function is created in the database holberton and the name of the function is SafeDiv.
--- The function takes two arguments: a and b (both are float).
--- The function returns a float.
--- The function is created with the following SQL statement:
 
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
