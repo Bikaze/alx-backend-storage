3379041d0f8f0a2294f3cb6bec3bd8f50369336a
Bikaze <clmntmugisha@gmail.com>
2024-07-17 23:38:40 +0200

    Update exercise.py with some new get functions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -33,3 +33,47 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(name=key, value=data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Get the data from Redis by key and convert it using the provided
+        function.
+
+        Parameters:
+        key (str): The key to retrieve the data from Redis.
+        fn (Optional[Callable]): A function to convert the data back to the
+        desired format.
+
+        Returns:
+        Union[str, bytes, int, float]: The data retrieved from Redis, converted
+        if a function is provided.
+        """
+        value = self._redis.get(name=key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Get the data from Redis by key and convert it to a string.
+
+        Parameters:
+        key (str): The key to retrieve the data from Redis.
+
+        Returns:
+        str: The data retrieved from Redis, converted to a string.
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        Get the data from Redis by key and convert it to an integer.
+
+        Parameters:
+        key (str): The key to retrieve the data from Redis.
+
+        Returns:
+        int: The data retrieved from Redis, converted to an integer.
+        """
+        return self.get(key, fn=int)
